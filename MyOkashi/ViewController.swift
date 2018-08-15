e038d0cfd14b1861e48580809e1aebc30fb09872
Kyonosuke Yamashita <bp16103@shibaura-it.ac.jp>
2018-08-15 18:15:46 +0900

    Finish

--- MyOkashi/ViewController.swift
+++ MyOkashi/ViewController.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import SafariServices
 
-class ViewController: UIViewController, UISearchBarDelegate, UITableViewDataSource {
+class ViewController: UIViewController, UISearchBarDelegate, UITableViewDataSource, UITableViewDelegate, SFSafariViewControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,6 +25,8 @@ class ViewController: UIViewController, UISearchBarDelegate, UITableViewDataSour
         // Table ViewのdataSourceを設定
         tableView.dataSource = self
         
+        // Table Viewのdelegateを設定
+        tableView.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -165,5 +168,22 @@ class ViewController: UIViewController, UISearchBarDelegate, UITableViewDataSour
         return cell
     }
     
+    // Cellが選択された際に呼び出されるdelegateメソッド
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // ハイライト解除
+        tableView.deselectRow(at: indexPath, animated: true)
+        // SFSafariViewを開く
+        let safariViewController = SFSafariViewController(url: okashiList[indexPath.row].link)
+        // delegateの通知先を自分自身
+        safariViewController.delegate = self
+        // SafariViewが開かれる
+        present(safariViewController, animated: true, completion: nil)
+    }
+    
+    // SafariViewが閉じられたときに呼ばれるdelegateメソッド
+    func safariViewControllerDidFinish(_ controller: SFSafariViewController) {
+        // SafariViewを閉じる
+        dismiss(animated: true, completion: nil)
+    }
 }
 
